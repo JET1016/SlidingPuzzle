88e2079965affb907c7dcbe2c59ef9754762496e
Loria Roie Grace N. Malingan <lnmalingan@up.edu.ph>
2018-05-21 10:54:09 +0800

    Added documentation

--- slidingPuzzle.c
+++ slidingPuzzle.c
@@ -1,3 +1,10 @@
+/*
+  CMSC 125 Project: Sliding Puzzle 
+  Description: A program that implements the game sliding puzzle inside ICS-OS.
+  Date Finished: May 18, 2018
+  Authors: Malingan, Loria Roie Grace N.
+           Torres, Jem E.
+ */
 #include "../../sdk/dexsdk.h"
 #include "../../sdk/time.h"
 
